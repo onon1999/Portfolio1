135c4ac6bbefc3eb9cc419ad92a7ad7b9c3421cf
rishabh-76 <rbrishabh76@gmail.com>
2025-02-05 12:58:27 +0800

    src modified for lottie

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,20 +1,44 @@
-"use client"
+// "use client"
 
+// import Lottie from "lottie-react";
+
+// const AnimationLottie = ({ animationPath, width }) => {
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: animationPath,
+//     style: {
+//       width: '95%',
+//     }
+//   };
+
+//   return (
+//     <Lottie {...defaultOptions} />
+//   );
+// };
+
+// export default AnimationLottie;
+
+
+"use client";
 import Lottie from "lottie-react";
 
 const AnimationLottie = ({ animationPath, width }) => {
+  // Ensure the component only renders on the client side
+  if (typeof window === "undefined") {
+    return null;
+  }
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: animationPath,
     style: {
       width: '95%',
-    }
+    },
   };
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
+  return <Lottie {...defaultOptions} />;
 };
 
 export default AnimationLottie;
